454d991055bdae96b270f85775df7699e9d2b6ef
Phungthevinh <ptvstar2003@gmail.com>
2025-09-30 10:59:47 +0700

    up

--- obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d73d145a1d2640b4cbaa40bc725732de1f4627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4402605ad2bbce777960e1d67a96412da9e07e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

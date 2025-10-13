e2f32cff1bc1af9520119107e1048ac7c87c7b53
Phungthevinh <ptvstar2003@gmail.com>
2025-10-13 10:48:27 +0700

    upload

--- routers/campaignsRouter.cs
+++ routers/campaignsRouter.cs
@@ -8,10 +8,16 @@ namespace WebAPI.routers
     {
         public campaignsRouter(WebApplication app)
         {
+            //tạo sự kiện giảm giá mới
             app.MapPost("/tao-su-kien-moi", (campaigns campaigns, dbContext dbContext) =>
             {
                 campaignsController taoSuKienMoi = new campaignsController(dbContext);
                 return taoSuKienMoi.taoSuKienDeCTVacpMa(campaigns);
+            });
+            //xem tất cả các sự kiện giảm giá
+            app.MapGet("/su-kien-giam-gia", (dbContext dbContext) =>
+            {
+
             });
         }
     }
